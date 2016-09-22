eb2b88d6a55ce323ece1309de3d234f09bc3f14d
Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
2016-09-22 22:56:29 +0300

    Change AppBundle to be just App.

---
+++ src/App/Controller/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Controller for the authentication routes.
+ *
+ * @package App\Controller
+ *
+ * @author  Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
+ */
+class AuthController extends Controller
+{
+}
