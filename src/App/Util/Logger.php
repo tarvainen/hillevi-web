76bfbf288506cb1ce2d6797d4fa53432bbc53255
Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
2016-10-01 21:37:39 +0300

    Add static logger to my own log file! No more $this->get('logger')->debug('something') and then you spend five minutes to find what you are looking for from the dev.log!!

---
+++ src/App/Util/Logger.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Util;
+
+/**
+ * A static logger. Finally!
+ *
+ * @package App\Util
+ *
+ * @author Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
+ */
+class Logger
+{
+    /**
+     * Logs the message to the debug.log file in the logs folder.
+     *
+     * @param mixed $msg
+     *
+     * @return void
+     */
+    public static function log($msg)
+    {
+        file_put_contents(__DIR__ . '/../../../var/logs/debug.log', print_r($msg . PHP_EOL, true), FILE_APPEND);
+    }
+}
