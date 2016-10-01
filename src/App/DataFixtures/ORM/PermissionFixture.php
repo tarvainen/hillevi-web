bb01eff3294255e1404a75ae373380e44fdb675b
Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
2016-10-01 12:40:53 +0300

    Add permission fixtures.

---
+++ src/App/DataFixtures/ORM/PermissionFixture.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures\ORM;
+
+use App\Entity\Permission;
+use App\Naming\Right;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Permission data fixtures to be preloaded to the database in the initialization.
+ *
+ * @package App\DataFixtures\ORM
+ *
+ * @author Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
+ */
+class PermissionFixture implements FixtureInterface
+{
+    /**
+     * Loads permissions data in to the database.
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // Define all loaded right modules here
+        $rights = [
+            Right::DASHBOARD,
+            Right::USER_SETTINGS,
+        ];
+
+        foreach ($rights as $right) {
+            $entity = new Permission();
+            $entity->setName($right);
+
+            $manager->persist($entity);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/App/Naming/Right.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Naming;
+
+/**
+ * Contains all the constants which has something to do with rights.
+ *
+ * @package App\Naming
+ *
+ * @author Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
+ */
+abstract class Right
+{
+    /**
+     * The right module constant values.
+     */
+    const DASHBOARD = 'dashboard';
+    const USER_SETTINGS = 'user_settings';
+}
