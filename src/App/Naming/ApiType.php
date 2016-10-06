8d469993c2e6468f46dfc1b0c1f279eccb18efda
Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
2016-10-06 19:11:46 +0300

    Add container for the api type constants.

---
+++ src/App/Naming/ApiType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Naming;
+
+/**
+ * Container for the API type constants
+ *
+ * @package App\Naming
+ *
+ * @author Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
+ */
+class ApiType
+{
+    const INNER  = 'inner';
+    const JSON   = 'json';
+    const XML    = 'xml';
+}
