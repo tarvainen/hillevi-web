5edee631006983cb08cbb778b9df11d3cbc419db
tarvainen <atte.tarvainen@pp1.inet.fi>
2016-11-03 19:34:15 +0200

    Add sql initializer file for configs.

---
+++ src/Sql/init.sql
@@ -0,0 +1,9 @@
+/**
+ * This file contains the necessary settings to be run
+ * in to the database before using this application. This
+ * file is run by the fixture data automatically so you don't need
+ * to do that by yourself.
+ */
+
+/** Set the event scheduler up and running */
+SET GLOBAL EVENT_SCHEDULER = ON;
