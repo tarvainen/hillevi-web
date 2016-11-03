abbdc7205390354a16382cbcb5622fed5b1479d9
tarvainen <atte.tarvainen@pp1.inet.fi>
2016-11-03 19:36:39 +0200

    Add db event for repeatically recalculating the inspection data open.

---
+++ src/Sql/Event/e_CalculateInspectionDataSummaries.sql
@@ -0,0 +1,34 @@
+/** Event for calculating inspection data summaries in the database without separated cron jobs. */
+
+DROP EVENT IF EXISTS e_CalculateInspectionDataSummaries;
+
+DELIMITER //
+
+CREATE EVENT e_CalculateInspectionDataSummaries
+  ON SCHEDULE EVERY 1 MINUTE STARTS NOW()
+DO BEGIN
+
+  DECLARE i         INT DEFAULT 0;
+  DECLARE n         INT DEFAULT 0;
+  DECLARE u         INT DEFAULT 0;
+  DECLARE startTime DATETIME;
+  DECLARE endTime   DATETIME;
+
+  -- We will call the procedure for the last 5 minutes
+  SET startTime = DATE_SUB(NOW(), INTERVAL 5 MINUTE);
+  SET endTime = NOW();
+
+  SELECT COUNT(id) FROM user INTO n;
+
+  -- Iterate through the users list
+  WHILE i < n DO
+    SELECT id FROM user LIMIT i,1 INTO u;
+
+    CALL sp_CalculateInspectionDataSummaries(u, startTime, endTime);
+
+    SET i = i + 1;
+  END WHILE;
+
+END //
+
+DELIMITER ;
