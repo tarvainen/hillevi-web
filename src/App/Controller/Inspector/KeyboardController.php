ce439572efb4b874a401844337bd527089d89e6a
tarvainen <atte.tarvainen@pp1.inet.fi>
2016-11-07 00:29:57 +0200

    Implement keyboard load data fetch for the given date range.

--- src/App/Controller/Inspector/KeyboardController.php
+++ src/App/Controller/Inspector/KeyboardController.php
@@ -98,4 +98,144 @@ class KeyboardController extends CController
 
         return new JsonResponse($data);
     }
+
+    /**
+     * Action to get the keyboard load data to be shown
+     * in the keyboard layout image.
+     *
+     * @Permission
+     *
+     * @Route("load")
+     * @Method("POST")
+     *
+     * @return JsonResponse
+     */
+    public function getKeyboardLoadAction()
+    {
+        list($startTime, $endTime) = $this->mapFromRequest(['startDate', 'endDate']);
+
+        if (!isset($startTime)) {
+            $startTime = new \DateTime('1990-01-01');
+        } else {
+            $startTime = new \DateTime($startTime);
+        }
+
+        if (!isset($endTime)) {
+            $endTime = new \DateTime('2036-01-01');
+        } else {
+            $endTime = new \DateTime($endTime);
+        }
+
+        $dql = '
+            SELECT
+              SUM(k.key_F1)           AS keyF1,
+              SUM(k.key_F2)           AS keyF2,
+              SUM(k.key_F3)           AS keyF3,
+              SUM(k.key_F4)           AS keyF4,
+              SUM(k.key_F5)           AS keyF5,
+              SUM(k.key_F6)           AS keyF6,
+              SUM(k.key_F7)           AS keyF7,
+              SUM(k.key_F8)           AS keyF8,
+              SUM(k.key_F9)           AS keyF9,
+              SUM(k.key_F10)          AS keyF10,
+              SUM(k.key_F11)          AS keyF11,
+              SUM(k.key_F12)          AS keyF12,
+              SUM(k.key_PrintScreen)  AS keyPrintScreen,
+              SUM(k.key_ScrollLock)   AS keyScrollLock,
+              SUM(k.key_Pause)        AS keyPause,
+              
+              SUM(k.key_1)            AS key1,
+              SUM(k.key_2)            AS key2,
+              SUM(k.key_3)            AS key3,
+              SUM(k.key_4)            AS key4,
+              SUM(k.key_5)            AS key5,
+              SUM(k.key_6)            AS key6,
+              SUM(k.key_7)            AS key7,
+              SUM(k.key_8)            AS key8,
+              SUM(k.key_9)            AS key9,
+              SUM(k.key_10)           AS key0,
+              SUM(k.key_Plus)         AS keyPlus,
+              SUM(k.key_Insert)       AS keyInsert,
+              SUM(k.key_Backspace)    AS keyBackspace,
+              SUM(k.key_Home)         AS keyHome,
+              SUM(k.key_PageUp)       AS keyPageUp,
+              SUM(k.key_NumLock)      AS keyNumLock,
+              
+              SUM(k.key_Tab)          AS keyTab,
+              SUM(k.key_Q)            AS keyQ,
+              SUM(k.key_W)            AS keyW,
+              SUM(k.key_R)            AS keyR,
+              SUM(k.key_T)            AS keyT,
+              SUM(k.key_Y)            AS keyY,
+              SUM(k.key_U)            AS keyU,
+              SUM(k.key_I)            AS keyI,
+              SUM(k.key_O)            AS keyO,
+              SUM(k.key_P)            AS keyP,
+              SUM(k.key_Tilde)        AS keyTilde,
+              SUM(k.key_Delete)       AS keyDelete,
+              SUM(k.key_End)          AS keyEnd,
+              SUM(k.key_PageDown)     AS keyPageDown,
+              
+              SUM(k.key_CapsLock)     AS keyCapsLock,
+              SUM(k.key_A)            AS keyA,
+              SUM(k.key_S)            AS keyS,
+              SUM(k.key_D)            AS keyD,
+              SUM(k.key_F)            AS keyF,
+              SUM(k.key_G)            AS keyG,
+              SUM(k.key_H)            AS keyH,
+              SUM(k.key_J)            AS keyJ,
+              SUM(k.key_K)            AS keyK,
+              SUM(k.key_L)            AS keyL,
+              SUM(k.key_OE)           AS keyOE,
+              SUM(k.key_AE)           AS keyAE,
+              SUM(k.key_Multiply)     AS keyMultiply,
+              
+              SUM(k.key_LeftShift)    AS keyLeftShift,
+              SUM(k.key_Z)            AS keyZ,
+              SUM(k.key_X)            AS keyX,
+              SUM(k.key_C)            AS keyC,
+              SUM(k.key_V)            AS keyV,
+              SUM(k.key_B)            AS keyB,
+              SUM(k.key_N)            AS keyN,
+              SUM(k.key_M)            AS keyM,
+              SUM(k.key_Comma)        AS keyComma,
+              SUM(k.key_Period)       AS keyPeriod,
+              SUM(k.key_Minus)        AS keyMinus,
+              SUM(k.key_RightShift)   AS keyRightShift,
+              SUM(k.key_Up)           AS keyUp,
+              
+              SUM(k.key_LeftCtrl)     AS keyLeftCtrl,
+              SUM(k.key_LeftAlt)      AS keyLeftAlt,
+              SUM(k.key_Space)        AS keySpace,
+              SUM(k.key_RightCtrl)    AS keyRightCtrl,
+              SUM(k.key_Left)         AS keyLeft,
+              SUM(k.key_Down)         AS keyDown,
+              SUM(k.key_Right)        AS keyRight
+
+            FROM App:KeyStroke k
+            WHERE
+             k.startTime >= :startTime AND
+             k.endTime <= :endTime AND
+             k.user = :userId
+            GROUP BY k.user
+        ';
+
+        /** @var Query $query */
+        $query = $this
+            ->manager()
+            ->createQuery($dql)
+            ->setParameter('startTime', $startTime->format(DateUtil::DATETIME_DB))
+            ->setParameter('endTime', $endTime->format(DateUtil::DATETIME_DB))
+            ->setParameter('userId', $this->getUserEntity()->getId());
+
+        $result = $query->getArrayResult();
+
+        if (empty($result)) {
+            $result = [];
+        } else {
+            $result = $result[0];
+        }
+
+        return new JsonResponse($result);
+    }
 }
