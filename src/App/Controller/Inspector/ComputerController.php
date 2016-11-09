3d31b63ffb3eed57046a8223bdb430f0ef84c5d8
tarvainen <atte.tarvainen@pp1.inet.fi>
2016-11-09 19:48:30 +0200

    Add computer controller base.

---
+++ src/App/Controller/Inspector/ComputerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Inspector;
+
+use App\Controller\CController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+/**
+ * Computer controller class for computer specific inspector data handling.
+ *
+ * @package App\Controller\Inspector
+ *
+ * @Route("api/inspector/computer/")
+ * @Method("POST")
+ *
+ * @author Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
+ */
+class ComputerController extends CController
+{
+
+}
