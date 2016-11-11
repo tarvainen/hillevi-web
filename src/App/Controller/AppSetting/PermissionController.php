4dd8161c310419693d7fb757956b26290bde89d0
tarvainen <atte.tarvainen@pp1.inet.fi>
2016-11-11 12:49:47 +0200

    Add bases for the PermissionController

---
+++ src/App/Controller/AppSetting/PermissionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\AppSetting;
+
+use App\Controller\CController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+/**
+ * Controller for the app setting permissions.
+ *
+ * @Route("api/appsetting/permissions/")
+ * @Method("POST")
+ *
+ * @package App\Controller\AppSetting
+ *
+ * @author Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
+ */
+class PermissionController extends CController
+{
+}
