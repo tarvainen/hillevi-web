6ec82346eabf2fa56dc7cfe9b4e2402d1111464f
tarvainen <atte.tarvainen@pp1.inet.fi>
2016-11-12 10:23:44 +0200

    Add the diagram trend controller base.

---
+++ src/App/Controller/Diagram/DiagramTrendController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Diagram;
+
+use App\Controller\CController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Controller for the diagram trend data.
+ *
+ * @package App\Controller\Diagram
+ *
+ * @Route("api/diagram/trend/")
+ * @Method("POST")
+ *
+ * @author Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
+ */
+class DiagramTrendController extends CController
+{
+}
