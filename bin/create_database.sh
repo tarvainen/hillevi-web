26b6c0f7440f3f52b4ea6a70ec8e08694da5bfbd
tarvainen <atte.tarvainen@pp1.inet.fi>
2016-11-12 10:21:36 +0200

    Update the database creation script.

--- bin/create_database.sh
+++ bin/create_database.sh
@@ -3,4 +3,5 @@
 php bin/console doctrine:database:create
 php bin/console doctrine:schema:create
 php bin/console doctrine:fixtures:load
+php bin/console doctrine:fixtures:load --fixtures=src/App/DataFixtures/Scripts --append
 php bin/console doctrine:migrations:migrate
